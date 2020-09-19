358e0e8a5430d69111866960f35e646e3d6c21bd
Vukan Mrvaljevic <vukanmrvaljevic@vukans-mbp.home>
2020-09-19 12:59:24 -0400

    track-covid responsive

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import CardComponent from "./Card/Card"
-import styles from "./cards.css"
+import "./cards.css"
 
 const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
@@ -9,21 +9,18 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className="container">
       <CardComponent
-        className={styles.infected}
         cardTitle="Infected"
         value={confirmed.value}
         lastUpdate={lastUpdate}
         cardSubtitle="Number of active cases from COVID-19."
       />
       <CardComponent
-        className={styles.recovered}
         cardTitle="Recovered"
         value={recovered.value}
         lastUpdate={lastUpdate}
         cardSubtitle="Number of recoveries from COVID-19."
       />
       <CardComponent
-        className={styles.deaths}
         cardTitle="Deaths"
         value={deaths.value}
         lastUpdate={lastUpdate}
